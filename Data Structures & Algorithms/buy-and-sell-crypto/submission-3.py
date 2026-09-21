57896c463ce84a6c7a0e2ab3d2e87d996042d764
Mel <huntermelverton@gmail.com>
2026-09-21 12:25:11 -0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0
+        right = 1
+        maxProfit = 0
+
+        while right < len(prices):
+            if prices[left] < prices[right]: # price right is way too small 
+                maxProfit = max(maxProfit, prices[right] - prices[left])
+            else: 
+                left = right
+            right += 1
+
+
+        return maxProfit
